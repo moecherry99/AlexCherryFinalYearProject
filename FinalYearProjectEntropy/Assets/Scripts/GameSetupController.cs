8f4aba8de68fb4b3788d68a09e80bb9d2f88c205
Alex Cherry <G00347106@gmit.ie>
2020-01-20 16:31:24 +0000

    Fixed Photon Issues

--- FinalYearProjectEntropy/Assets/Scripts/GameSetupController.cs
+++ FinalYearProjectEntropy/Assets/Scripts/GameSetupController.cs
@@ -16,7 +16,7 @@ public class GameSetupController : MonoBehaviour
     private void CreatePlayer()
     {
         Debug.Log("Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("Photon Prefabs", "Photon Player"), Vector3.zero, Quaternion.identity);
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity);
     }
 
     void Update()
