d0d6fce70c9201e8e95361bbe3e9815981b81fd2
Alex Cherry <G00347106@gmit.ie>
2020-02-08 01:04:49 +0000

    Movement added

---
+++ FinalYearProjectEntropy/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Reference : https://www.youtube.com/watch?v=_QajrabyTJc&t=324s
+// Script used for looking around with the first person character
+public class MouseLook : MonoBehaviour
+{
+
+    // Change speed of sensitivity
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Locks Cursor to middle of screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // disables rotation to wrong side (locks on 180 degrees up and down)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // rotates when you move mouse
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
--- FinalYearProjectEntropy/Assets/Scripts/PlayerMovement.cs
+++ FinalYearProjectEntropy/Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Script for movement
+// Reference : https://www.youtube.com/watch?v=_QajrabyTJc&t=324s
+
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    public CharacterController controller;
+
+    // set speed
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    // variables for ground check
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+
+
+    bool isGrounded;
+    Vector3 velocity;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey("a"))
-        {
 
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        // ground check to reset velocity
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
         }
+        // creates movement based on direction you are facing with mouse look script
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+
+        controller.Move(move * speed * Time.deltaTime);
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
     }
 }
