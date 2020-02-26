f8b76d1b4e08f3909509b032ea0481bd5cab9e00
Alex Cherry <G00347106@gmit.ie>
2020-02-26 12:43:42 +0000

    Added quick stats menu with toggle switch

---
+++ FinalYearProjectEntropy/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Script for the Character Stats page
+// Reference : https://www.youtube.com/watch?v=aN11LnlF89I
+public class MenuManager : MonoBehaviour
+{
+
+    // game object options menu
+    public GameObject optionsMenu;
+
+    // referencing player health 
+    public PlayerHealthScript playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if press C
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            // always sets to opposite (true or false)
+            optionsMenu.gameObject.SetActive(!optionsMenu.gameObject.activeSelf);
+        }
+        
+    }
+}
