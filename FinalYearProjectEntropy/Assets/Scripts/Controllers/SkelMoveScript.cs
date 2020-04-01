770fb7a9a26f240288b8674b7fabcf83907a0f0f
Alex Cherry <G00347106@gmit.ie>
2020-04-02 00:13:39 +0100

    started enemy animation and LaTeX document

---
+++ FinalYearProjectEntropy/Assets/Scripts/Controllers/SkelMoveScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkelMoveScript : MonoBehaviour
+{
+    private Vector3 target = new Vector3(5.0f, 0.0f, 0.0f);
+
+    void Update()
+    {
+        // Spin the object around the world origin at 20 degrees/second.
+        transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime);
+    }
+}
